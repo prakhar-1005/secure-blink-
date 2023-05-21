86833b2eec3776c2b930675ca208144a4259e9b5
Prakhar Pandey <prakhar10may@gmail.com>
2023-05-21 19:29:08 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import About from './components/About.vue';
+import Services from './components/Services.vue';
+import Technologies from './components/Technologies.vue';
+import Contact from './components/Contact.vue';
+
+</script>
+
+<template>
+  <div style="backgroundImage:url('./src/assets/back.svg')" class="h-auto w-full bg-contain bg-no-repeat">
+    <Navbar/> 
+    <Hero/>
+    <About/>
+    <Services/>
+    <Technologies/>
+    <Contact/>
+    <RouterView />
+  </div>
+</template>
+
